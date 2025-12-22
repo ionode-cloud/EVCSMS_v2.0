70ff4a9dc0203d3e5a666061ff31812e4c62095a
Pupunfzx <beheraj542@gmail.com>
2025-12-22 22:52:25 +0530

    fix: add dotenv dependency for render

--- EV-CSM/EV-backend/server.js
+++ EV-CSM/EV-backend/server.js
@@ -1,11 +1,12 @@
+import dotenv from "dotenv";
+
+dotenv.config();
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import crypto from "crypto";
 import { Cashfree, CFEnvironment } from "cashfree-pg";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const app = express();
 app.use(express.json());
